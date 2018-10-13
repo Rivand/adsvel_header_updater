41d3bd6a53c75254cd51a952913a131c45db8a02
RivandBlack <RivandBlack@yandex.ru>
2018-10-14 00:56:56 +0500

    v 0.0.6
    - 'FileUpdater::UpdateFile()' now finds the @data field and updates it if required.

--- file_updater.cpp
+++ file_updater.cpp
@@ -6,17 +6,72 @@
  * @brief    Class-singleton that updates file header if it is doxygen compatible.
  ************************************************************************************************************************************************************/
 #include "file_updater.h"
+#include <boost/filesystem.hpp>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
-using ::std::pair;
-using ::std::string;
+#include <regex>
+#include <sstream>
+#include <vector>
 using ::std::ifstream;
+using ::std::ofstream;
+using ::std::ostringstream;
+using ::std::pair;
+using ::std::regex;
+using ::std::setw;
+using ::std::string;
+using ::std::vector;
 pair<bool, string> FileUpdater::UpdateFile(const std::string& in_file_name, const std::string& in_version_name) {
     pair<bool, std::string> answer{false, "Unknown state"};
-    ifstream source_file(in_file_name, ifstream::in);
-    if (!source_file.is_open()){
+    ifstream in_source_file(in_file_name, ifstream::in);
+    if (!in_source_file.is_open()) {
         answer.second = "can't open file.";
         return answer;
     }
+    // Get the last modified time of the file.
+    time_t tt = boost::filesystem::last_write_time(in_file_name);
+    tm t = *std::localtime(&tt);
+    ostringstream oss_last_write_time;
+    oss_last_write_time << std::setfill('0') << setw(2) << t.tm_mday << "." << setw(2) << t.tm_mon << "." << 1900 + t.tm_year << " " << setw(2) << t.tm_hour << ":" << setw(2) << t.tm_min << ":" << setw(2) << t.tm_sec
+                        << std::flush << std::flush << std::endl;
+    string last_write_time_str_real{oss_last_write_time.str()};
+    // Read the file.
+    vector<string> source_file_content;
+    string str{};
+    while (std::getline(in_source_file, str)) {
+        source_file_content.push_back(std::move(str));
+    }
+    in_source_file.close();
+    // Find the line with the date.
+    string last_write_time_str_doxy{};
+    for (auto& c : source_file_content) {
+        std::smatch find_match;
+        if (std::regex_search(c.cbegin(), c.cend(), find_match, regex(".*?@date[\\s\\t]+?\\d"))) {
+            last_write_time_str_doxy = string(c.begin() + find_match.length() - 1, c.end());
+            if (!last_write_time_str_doxy.compare(last_write_time_str_real)) {  // The dates and times are the same and you do not need to update the file.
+                answer.second = "no file update required.";
+                return answer;
+            } else { // Update the line with date and time.
+                c = string(c.begin(), c.begin() + find_match.length() - 1);
+                c.append(last_write_time_str_real);
+                c.pop_back();
+            }
+        };
+    }
+    if (!last_write_time_str_doxy.size()) {  // Are unable to find the row with the field "@data".
+        answer.second = "could not find date field.";
+        return answer;
+    }
+
+    // Find the line with the name of the version and replace it if necessary.
+
+    // Overwrite file.
+    std::ofstream out_source_file(in_file_name);
+    for (const auto& c: source_file_content) out_source_file << c << std::endl;
+    out_source_file.close();
+    // Set the time of the last file change is the same as now specified in the doxygen description.
+    boost::filesystem::last_write_time(in_file_name, tt);
+
+    answer.second = "file has been updated.";
     return answer;
 }
